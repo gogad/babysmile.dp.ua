1a0e4a58ff325481cd29338cd7c974f8b86fb9d9
darkside <andrey.garbuz@gmail.com>
2011-11-08 00:30:36 +0200

    Add some libraries and modify .gitignore file

---
+++ smarty/libs/plugins/modifier.convert_currency.php
@@ -0,0 +1,18 @@
+<?php
+
+function smarty_modifier_convert_currency($value)
+{
+	$default_currency = db::getDB()->selectRow('SELECT rate,name FROM currency WHERE `default` = 1');
+    if (!isset($_SESSION['currency'])) {
+    	$currency = $default_currency;
+    } else {
+    	$currency = db::getDB()->selectRow('SELECT rate,name FROM currency WHERE id = ?d',$_SESSION['currency']);
+    }
+    
+    if (!$currency) $currency = $default_currency;
+    
+    return round($value/$currency['rate'],2).'&nbsp;'.$currency['name'];
+}
+
+
+?>
